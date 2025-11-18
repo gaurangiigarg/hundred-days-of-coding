51c86bd8f610a4adcb6c2d7c18933057a4df59cb
gaurangiigarg <gaurangigg12@gmail.com>
2025-11-18 23:30:05 +0530

    Add files via upload

---
+++ day22q44.c
@@ -0,0 +1,21 @@
+//Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+#include <stdio.h>
+int main() {
+    
+    int n, i;
+    float sum = 0.0;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    sum = 1.0;  
+
+    for(i = 2; i <= n; i++) {
+    sum += (2.0*i - 1) / (2.0*i);
+}
+
+
+    printf("Approximate sum: %.1f\n", sum);
+
+    return 0;
+}
